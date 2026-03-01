f2172a65fa250648a649494da117110991db5805
Filipe <filiperosso99@gmail.com>
2026-02-28 22:28:38 -0300

    feat: add fastify api

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
@@ -5,7 +8,10 @@ import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig([
+  { ignores: ["**/*.d.ts"] },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
@@ -13,6 +19,23 @@ export default defineConfig([
     languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+  {
+    files: ["**/*.{ts,mts,cts}"],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
   eslintConfigPrettier,
   {
     plugins: {
